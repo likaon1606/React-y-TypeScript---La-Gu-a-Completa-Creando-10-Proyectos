0bb0bb939af6dcf2294873cc3fc5d4c1e147876d
Ariel Fuentes <ariel.fuentesgarcia@gmail.com>
2024-07-02 18:16:01 -0600

    .map ARRAYS

---
+++ SECCION_2/IntroduccionJS/IntroduccionJS/js/09.js
@@ -0,0 +1,8 @@
+// ARRAYS. El listado siempre debe ser acorde, nunca mezclado
+
+const tecnologias = [
+    20, 30, 40, true, "React.js"
+]
+
+console.table(tecnologias);
+console.log(tecnologias[1]);
---
+++ SECCION_2/IntroduccionJS/IntroduccionJS/js/10.js
@@ -0,0 +1,38 @@
+// OPERACIONES CON ARRAYS
+
+const tecnologias = ['HTML', 'CSS', 'JavaScrtipt', 'React.js', 'Node.js']
+
+//* Agregar un elemento al final del array
+//! No se recomienda usar push en React.js porque modifica el array
+
+// tecnologias.push('Nest.js')
+// console.table(tecnologias);
+
+//* esto es mejor
+const nuevoArray = [...tecnologias, 'Nest.js']
+// console.table(nuevoArray);
+
+//* "shift()" Elimina el primer elemento de un array
+//! También muta el ARRAY
+tecnologias.shift()
+// console.table(tecnologias)
+
+//* esto es mejor
+const tecnologias2 = tecnologias.filter((tech) => {
+    if(tech !== 'HTML'){
+        return tech
+    }
+})
+
+// console.log(tecnologias2)
+
+
+// .map NO modifica el ARRAY
+const tecnologias3 = tecnologias.map(function(tech) {
+    if(tech === 'Node.js'){
+        return 'Nest.js'
+    } else {
+        return tech
+    }
+})
+console.log(tecnologias3);
